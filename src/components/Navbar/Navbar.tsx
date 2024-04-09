91902db8f1da451871b3972980384de0d9f69a74
Omkar Patil <git21cs072-t@students.git.edu>
2024-04-09 15:05:15 +0530

    changed for mobile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,8 +3,36 @@ import { FaClock } from 'react-icons/fa';
 import Timer from '../Timer/Timer';
 
 const Navbar = () => {
+  const isSmallScreen = window.innerWidth <= 480; // Define a threshold for small screens
   return (
-    <div className='flex flex-row bg-[#fadfb4] '>
+
+
+    <>
+    {
+      isSmallScreen?(
+        <div className='flex flex-row bg-[#fadfb4] '>
+      
+        <FaClock className="ml-3 mt-6  opacity-65 text-2xl " />
+        
+      
+      <div className="mt-3 p-2 mb-3 ml-3 items-center">
+      <p className='text-lg font-bold' >Discount expires in </p>
+      
+      </div>
+      <div className="mt-3 p-2 mb-3  items-center"><Timer/></div>
+      
+
+      
+      <button className=' hover:scale-105 duration-150 bg-[#e89ba4] w-auto h-10 font-bold rounded-xl mt-3 mb-3 p-2'>Claim Discount</button>
+    
+     
+      
+      
+    </div>
+
+      )
+      :
+      <div className='flex flex-row bg-[#fadfb4] '>
       <div className="flex mt-3 mb-3 ml-5 items-center bg-[#f5c67a] rounded-full">
         <FaClock className="ml-3 opacity-65 " />
         <p className='p-2 font-bold text-[#594433] mr-2'>One-time Offer</p>
@@ -22,6 +50,12 @@ const Navbar = () => {
       
       
     </div>
+
+    }
+    
+    
+    </>
+    
   );
 };
 
